164de5b0e09a6db7ba45fd074019a732c76f1325
onkarpatole <onkarpatole28@gmail.com>
2024-11-10 17:23:46 -0500

    Changed layout and also page.

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,18 @@
-import localFont from "next/font/local";
+import { Cormorant_Upright, Open_Sans } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const cormorant_upright = Cormorant_Upright({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-cormorant_upright",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+
+const open_sans = Open_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800"],
+  variable: "--font-open_sans",
 });
 
 export const metadata = {
@@ -20,10 +23,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${cormorant_upright.variable} ${open_sans.variable}`}>
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
